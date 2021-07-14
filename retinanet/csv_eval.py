1495a9ee516f6a15d85e187f1f9e02abf58c3226
Vaishnav Potlapalli <51985757+va1shn9v@users.noreply.github.com>
2021-07-15 03:15:20 +0530

    changed to len

--- retinanet/csv_eval.py
+++ retinanet/csv_eval.py
@@ -122,7 +122,7 @@ def _get_detections(dataset, retinanet, score_threshold=0.05, max_detections=100
                 for label in range(dataset.num_classes()):
                     all_detections[index][label] = image_detections[image_detections[:, -1] == label, :-1]
                     if label == 0:
-                        print("Len of all Detections : {}".format(all_detections[index][label]))
+                        print("Len of all Detections : {}".format(len(all_detections[index][label])))
             else:
                 # copy detections to all_detections
                 for label in range(dataset.num_classes()):
